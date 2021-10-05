3ee82323da1878c8b459d5e4c24f1c902af799e6
Sampath Kumar Reddy <ssampathkumarredd@deloitte.com>
2021-10-05 18:13:54 +0530

    basic code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import _ from 'lodash';
+import styled from 'styled-components';
+import list from './common/list.json';
+import data from './common/learning_tools.json';
+import videoData from './common/video.json';
+
+const List = styled.li`
+    padding:8px;
+    cursor:pointer;
+    &:hover{
+      background-color: #ed1233;
+    }
+`;
 
 function App() {
+  const [listData, setListData] = useState({});
+
+  useEffect(() => {
+    let data = list.data;
+    console.log(_.groupBy(data, "type"));
+    setListData(_.groupBy(data, "type"));
+  }, [])
+  let url = videoData.data.attributes.field_video.und[0].video_url;
+  url = url.replace("watch?v=", "embed\/")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ margin: 20 }}>
+        {Object.keys(listData || []).map((section, index) => {
+          return (
+            <div >
+              <h1>{section}</h1>
+              <ul key={index} style={{ border: '1px solid black', width: 170, textAlign: "center", padding: 24, listStyleType: 'none' }}>
+                {
+                  listData[section].map((list => {
+                    return <List key={list.id}>{list.attributes.title}</List>;
+                  }))
+                }
+              </ul>
+            </div>)
+        })
+        }
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: data.data.attributes.body.und[0].value }}></div>
+      <div dangerouslySetInnerHTML={{ __html: videoData.data.attributes.body.und[0].value }}></div>
+      <div>
+        <iframe
+          width="853"
+          height="480"
+          src={url}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </div>
+    </div >
   );
 }
 
